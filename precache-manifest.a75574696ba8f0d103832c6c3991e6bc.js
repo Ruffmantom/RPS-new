78a94f5000c51060e956ea9cc0adea9847c88097
ruffmantom <tomruffcodes@gmail.com>
2020-06-29 11:56:25 -0600

    Updates

---
+++ precache-manifest.a75574696ba8f0d103832c6c3991e6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80dd81b7a633229b55f2e6c32d38485",
+    "url": "/RPS-new/index.html"
+  },
+  {
+    "revision": "5dcc6fdf43308692ba7f",
+    "url": "/RPS-new/static/css/main.1bf60a4d.chunk.css"
+  },
+  {
+    "revision": "4eda5984772a8d7abb6a",
+    "url": "/RPS-new/static/js/2.ef3e4d1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RPS-new/static/js/2.ef3e4d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcc6fdf43308692ba7f",
+    "url": "/RPS-new/static/js/main.f8fc4be1.chunk.js"
+  },
+  {
+    "revision": "f95ea3a70ff8a7e7987e",
+    "url": "/RPS-new/static/js/runtime-main.b0c4aeef.js"
+  }
+]);
